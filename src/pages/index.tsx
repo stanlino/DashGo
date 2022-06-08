386102fafb935f94cea2c95ca6c8a3cc5ba4c775
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-08 02:56:56 -0300

    feat: create SignIn Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,52 @@
-export default function Home() {
+import { 
+  Button, 
+  Flex, 
+  FormControl,
+  FormLabel, 
+  Stack 
+} from "@chakra-ui/react";
+import { Input } from "../components/form/input";
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w='100vw'
+      h='100vh'
+      align='center'
+      justify='center'
+    >
+      <Flex
+        as='form'
+        w='100%'
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir='column'
+      >
+        <Stack spacing='4'>
+
+          <Input 
+            type='email'
+            name='email'
+            label='E-mail'
+          />
+
+          <Input 
+            type='password'
+            name='password'
+            label='Password'
+          />
+
+          <Button
+            type='submit'
+            size='lg'
+            colorScheme="pink"
+          >
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   )
 }
