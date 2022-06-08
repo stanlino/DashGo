f71f0fa8f499dcd0871b0d055051cc1dceb90578
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-08 03:21:40 -0300

    feat: update header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,18 @@
-import { Flex, Input, Text, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { 
+  Flex, 
+  Input, 
+  Text, 
+  Icon,
+  HStack,
+  Box,
+  Avatar
+} from "@chakra-ui/react";
+
+import { 
+  RiSearchLine, 
+  RiNotificationLine,
+  RiUserAddLine
+} from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -49,6 +62,36 @@ export function Header() {
           mr='4'
         />
         <Icon as={RiSearchLine} fontSize='20' />
+      </Flex>
+
+      <Flex
+        align='center'
+        ml='auto'
+      >
+        <HStack 
+          spacing='8' 
+          mx='8'
+          pr='8'
+          py='1'
+          color='gray.300'
+          borderRightWidth={1}
+          borderColor='gray.700'
+        >
+          <Icon as={RiNotificationLine} fontSize='20' />
+          <Icon as={RiUserAddLine} fontSize='20' />
+        </HStack>
+      </Flex>
+
+      <Flex align='center'>
+        <Box mr='4' textAlign='right'>
+          <Text>Stanley Filipe</Text>
+          <Text color='gray.300' fontSize='sm'>stanley92filipe@gmail.com</Text>
+        </Box>
+        <Avatar 
+          size='md'
+          name='Stanley Filipe'
+          src='https://github.com/stanlino.png'
+        />
       </Flex>
     </Flex>
   )
