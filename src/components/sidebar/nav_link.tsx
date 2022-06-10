dd0ee48345544edb7eae78d98182f8b9695320d1
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-10 00:47:57 -0300

    feat: flag current page in sidebar

--- src/components/sidebar/nav_link.tsx
+++ src/components/sidebar/nav_link.tsx
@@ -1,6 +1,6 @@
 import { Icon, Link as ChakraLink, LinkProps as ChackraLinkProps, Text } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from 'next/link'
+import { ActiveLink } from "../activeLink";
 
 interface NavLinkProps extends ChackraLinkProps{
   icon: ElementType
@@ -10,11 +10,11 @@ interface NavLinkProps extends ChackraLinkProps{
 
 export function NavLink({ children, icon, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display='flex' alignItems='center' {...rest}>
         <Icon as={icon} fontSize='20' />
         <Text ml='4' fontWeight='medium'>{children}</Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   )
 }
