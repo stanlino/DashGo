7608b8b34ecf045517abe31b1b4835100b21f928
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-17 16:04:43 -0300

    feat: add api user listing

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,18 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data
+    const users = data.users.map(user => {
+      return {
+        ...user,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    return users
   })
 
   const isWideVersion = useBreakpointValue({
@@ -62,85 +73,37 @@ export default function UserList() {
                     </Th>
                     <Th>Usuário</Th>
                     {isWideVersion && <Th>Data de cadastro</Th>}
-                    <Th w='0'></Th>
+                    {isWideVersion && <Th w='0'>Ações</Th>}
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td px={['4', '4', '6']}>
-                      <Box>
-                        <Text fontWeight='bold'>Stanley Filipe</Text>
-                        <Text fontSize='small' color='gray.300'>stanley92filipe@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril 2022</Td>}
-                    <Td>
-                      {isWideVersion && (
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                        >
-                          Editar
-                        </Button>
-                      )}
-                    </Td>
-                  </Tr>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td px={['4', '4', '6']}>
-                      <Box>
-                        <Text fontWeight='bold'>Stanley Filipe</Text>
-                        <Text fontSize='small' color='gray.300'>stanley92filipe@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril 2022</Td>}
-                    <Td>
-                      {isWideVersion && (
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                        >
-                          Editar
-                        </Button>
-                      )}
-                    </Td>
-                  </Tr>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td px={['4', '4', '6']}>
-                      <Box>
-                        <Text fontWeight='bold'>Stanley Filipe</Text>
-                        <Text fontSize='small' color='gray.300'>stanley92filipe@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril 2022</Td>}
-                    <Td>
-                      {isWideVersion && (
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                        >
-                          Editar
-                        </Button>
-                      )}
-                    </Td>
-                  </Tr>
+                  { data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
+                      </Td>
+                      <Td px={['4', '4', '6']}>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='small' color='gray.300'>{user.email}</Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                      <Td>
+                        {isWideVersion && (
+                          <Button
+                            as='a'
+                            size='sm'
+                            fontSize='sm'
+                            colorScheme='purple'
+                            leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                          >
+                            Editar
+                          </Button>
+                        )}
+                      </Td>
+                    </Tr>
+                  )) }
                 </Tbody>
               </Table>
 
