9a23af4d042cbd448a6cbc3a5e2116dcf7047388
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-17 17:20:08 -0300

    feat: update Pagination component

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,7 +1,40 @@
-import { Box, Stack } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import { Item } from "./item";
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number
+  registerPerPage?: number
+  currentPage?: number
+  onPageChange: (page: number) => void
+}
+
+const siblingsCount = 1
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1
+    })
+    .filter(page => page > 0)
+}
+
+export function Pagination({ 
+  totalCountOfRegisters,
+  currentPage = 1,
+  onPageChange,
+  registerPerPage = 10
+}: PaginationProps) {
+
+  const lastPage = Math.floor(totalCountOfRegisters / registerPerPage)
+
+  const previousPages = currentPage > 1
+    ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+    : []
+
+  const nextPages = currentPage < lastPage
+    ? generatePagesArray(currentPage, Math.min(currentPage + siblingsCount, lastPage))
+    : []
+
   return (
     <Stack
       direction={['column','row']}
@@ -14,10 +47,24 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction='row' spacing='2'>
-        <Item nummber={1} isCurrent />
-        <Item nummber={2} />
-        <Item nummber={3} />
-        <Item nummber={4} />
+        
+        {currentPage > (1 + siblingsCount) && <Item nummber={1} />}
+
+        { currentPage > (2 + siblingsCount) && <Text color='gray.300' width='8' textAlign='center'>...</Text> }
+
+        {previousPages.length > 0 && previousPages.map(page => {
+          return <Item key={page} nummber={page} />
+        })}
+
+        <Item nummber={currentPage} isCurrent />
+      
+        {nextPages.length > 0 && nextPages.map(page => {
+          return <Item key={page} nummber={page} />
+        })}
+
+        { (currentPage + 1 + siblingsCount) < lastPage && <Text color='gray.300' width='8' textAlign='center'>...</Text> }
+
+        {(currentPage + siblingsCount) < lastPage && <Item nummber={lastPage} />}
       </Stack>
     </Stack>
   )
